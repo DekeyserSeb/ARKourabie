1c0c9e48ad42fb1b7f723e0fbc85eb2e70ce6f52
Geoff_Ish <delta-top@hotmail.com>
2019-03-29 13:06:58 +0100

    Animation prête ! Yeahhh

---
+++ Assets/Run_Anim.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run_Anim : MonoBehaviour
+{
+    public AnimationClip Break_Animation;
+
+    private void OnMouseDown()
+    {
+
+    }
+}
