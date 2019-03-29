0eb03b12d75d9591bb1ccfec968285bdcd4815aa
Geoff_Ish <delta-top@hotmail.com>
2019-03-29 14:10:46 +0100

    Animation cliquable ! Mais pas encore redéclenchable...

---
+++ Assets/Scripts/animationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.GetComponent<Animator>();    
+    }
+
+ 
+
+    private void OnMouseDown()
+    {
+        anim.Play("Kourabie_Break");
+        //anim.ResetTrigger("Kourabie_Break");
+    }
+}
