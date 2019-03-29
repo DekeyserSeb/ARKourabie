d902c0c0c469dfc23ff3f857b2c98b209d296363
Geoff_Ish <delta-top@hotmail.com>
2019-03-29 18:12:42 +0100

    Préparation pour le "Reverse_Broken" !

--- Assets/Scripts/animationController.cs
+++ Assets/Scripts/animationController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class animationController : MonoBehaviour
 {
     public Animator anim;
-    
+    bool state = false; //=true when already broken, = false when it can be break
+
     // Start is called before the first frame update
     void Start()
     {
-        anim.GetComponent<Animator>();    
+        anim.GetComponent<Animator>();
     }
 
     private void OnMouseDown()
     {
-        anim.Play("Kourabie_Break");
-        anim.Play("Default_State");
+        if (!state)
+        {
+            anim.Play("Kourabie_Break");
+            anim.Play("Default_State");
+        }
+        else
+        {
+
+            anim.Play("Kourabie_Break_Reverse");
+            anim.Play("Default_State");
+        }
     }
 }
