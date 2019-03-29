fdbcc94b8b2be507e952be182da3fb08073c1326
Geoff_Ish <delta-top@hotmail.com>
2019-03-29 10:35:38 +0100

    Crack_Kourabie fonctionnel (Mesh Collider match position avec Kourabie)

---
+++ Assets/Crack_Kourabie.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crack_Kourabie : MonoBehaviour
+{
+    public GameObject brokenVersion;
+
+    private void OnMouseDown()
+    {
+        //Instantiate(brokenVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        brokenVersion.SetActive(true);
+    }
+}
