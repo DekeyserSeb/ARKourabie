6f2d96c2536157c9c40ab086608ce1a4b088ca30
Geoff_Ish <delta-top@hotmail.com>
2019-04-13 22:53:15 +0200

    QR-Code fully implemented with Switch Mode Button ! Needed scripts updates to launch toggle active after Vuforia Start + Check if != null to avoid any crash and bug (was hard to figure it out but I did it :D)

--- Assets/Scripts/Start_AR.cs
+++ Assets/Scripts/Start_AR.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
 
 public class Start_AR : MonoBehaviour
 {
     public GameObject Kourabie;
     public GameObject Kourabie_Broken;
     public GameObject Sugar_Particules;
+    public GameObject ImageTarget_Top;
+    public GameObject ImageTarget_Bottom;
+    public GameObject ImageTargetQR;
+    public Toggle toggle;
+
+    private bool mVuforiaStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if (vuforia != null)
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+    }
+
+
+    void StartAfterVuforia()
+    {
+        mVuforiaStarted = true;
+        if (Kourabie !=null && Kourabie_Broken != null && Sugar_Particules != null && ImageTarget_Top != null && ImageTarget_Bottom != null && ImageTargetQR != null && toggle != null)
+        {
+        //all true to initialize correctly the AR Camera with all targets
+        ImageTargetQR.SetActive(true);
+        ImageTarget_Top.SetActive(true);
+        ImageTarget_Bottom.SetActive(true);
+
+        //to get only the good objects actives
         Kourabie.SetActive(true);
         Kourabie_Broken.SetActive(false);
         Sugar_Particules.SetActive(false);
+
+        //toggle.isOn = false;
+        //to start with Kourabie Box image target
+        toggle.isOn = true;
+        Debug.Log("Toggle is On thanks to the script Start_AR");
+        }
     }
 }
---
+++ Assets/Scripts/Toggle_SetActive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toggle_SetActive : MonoBehaviour
+{
+    public Toggle toggle;
+    public GameObject Kour_Whole;
+    public GameObject Kour_Broken;
+    public GameObject Sugar;
+
+    public void ToggleSet()
+    {
+        if (Kour_Whole != null && Kour_Broken != null && Sugar != null)
+        {
+            if (toggle.isOn)
+            {
+                this.gameObject.SetActive(false);
+                //Debug.Log("QR_Code Target deactivated");
+            }
+            else
+            {
+                this.gameObject.SetActive(true);
+                Kour_Whole.SetActive(true);
+                Kour_Broken.SetActive(false);
+                Sugar.SetActive(false);
+            }
+        }
+    }
+
+}
