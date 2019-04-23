f472258d1fb0b1aad361a273efbd2bf4b8f8e907
Dekeyser Sébastien <sebastien.dekeyser97@gmail.com>
2019-04-23 02:47:31 +0200

    J'ai ajouté un lien a ton bouton purchase
    car il menait a rien mais tu peux le rechangé si quelque chose d'autre était prévu à la base

---
+++ Assets/Link.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClickButton()
+    {
+        Application.OpenURL("https://www.chrisanthidis.gr/en/products/delights/traditional-kourabie/kourabie-with-whole-almonds-metal-box-450g");
+    }
+
+
+}
